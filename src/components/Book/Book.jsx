a0b5d62997ae037fc67c3e3af064515b815fa864
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-01-05 23:46:35 +0530

    code added

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -10,8 +10,11 @@ const Book = ({ book }) => {
         </figure>
         <div className="card-body">
           <div className="flex justify-center gap-4">
-            {tags.map((tag) => (
-              <button className="btn btn-xs bg-green-100 text-green-600">
+            {tags.map((tag, index) => (
+              <button
+                key={index}
+                className="btn btn-xs bg-green-100 text-green-600"
+              >
                 {tag}
               </button>
             ))}
--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetail = () => {
   const { bookId } = useParams();
-
+  const data = useLoaderData();
+  const id = parseInt(bookId);
+  const book = data.find((book) => book.bookId === id);
+  console.log(book);
   return (
     <div>
       <h2>Book details</h2>
