e52b6c9067c8ae50e653f9fbe92e103d72389248
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-01-07 17:02:53 +0530

    added js file

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -5,11 +5,23 @@ const BookDetail = () => {
   const { bookId } = useParams();
   const data = useLoaderData();
   const id = parseInt(bookId);
+
   const book = data.find((book) => book.bookId === id);
-  console.log(book);
+  const { bookId: currentBookId, image, author } = book;
+
+  const handleMarkAsRead = () => {};
   return (
-    <div>
-      <h2>Book details</h2>
+    <div className="my-12">
+      <h2>Book details {bookId}</h2>
+      <img className="w-36" src={image} alt="" />
+      <br />
+      <button
+        onClick={handleMarkAsRead}
+        className="btn btn-outline mr-5 btn-primary"
+      >
+        Mark As Read
+      </button>
+      <button className="btn btn-outline btn-accent">Add To Wishlist</button>
     </div>
   );
 };
